599822542bc3cb6eb5ec41eaf26d67c8c60cd52f
wmacnab <william.macnab@yahoo.com>
2022-08-22 21:53:24 -0400

    Chapter 1 complete

---
+++ ch1.py
@@ -0,0 +1,30 @@
+from tempfile import TemporaryFile
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from  bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTPTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
+
+"""
+When writing scrapers, it’s important to think about the overall pattern of your code
+in order to handle exceptions and make it readable at the same time. You’ll also likely
+want to heavily reuse code. Having generic functions such as getSiteHTML and
+getTitle (complete with thorough exception handling) makes it easy to quickly—
+and reliably—scrape the web.
+"""
--- hello.py
+++ hello.py
@@ -1,4 +1,24 @@
+#from turtle import ht
+"""
+import imp
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+try:
+    html = urlopen('http://pythonscraping.com/pages/page1.html') # returns file object
+except HTTPError as e:
+    print(e)
+    # return null, break, or do some other "Plan B"
+except URLError as e:
+    print('The server could not be found!')
+else:
+    print('It Worked!')
+    # program continues. Note: If you return or break in the exception catch, you do not need to use the "else" statement
+
+# a BeautifulSoup object with parameters HTML text object, and the second is the parser bs will use
+#bs = BeautifulSoup(html.read(), 'html.parser')
+bs = BeautifulSoup(html, 'html.parser')
+print(bs)
+"""
